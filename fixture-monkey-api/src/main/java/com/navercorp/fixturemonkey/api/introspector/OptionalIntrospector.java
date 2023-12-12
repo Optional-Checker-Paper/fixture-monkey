c23de75262ac61a21184d090d8f81c2854e219dd
Michael Ernst <mernst@cs.washington.edu>
2023-12-11 19:50:46 -0800

    Change labels

--- fixture-monkey-api/src/main/java/com/navercorp/fixturemonkey/api/introspector/OptionalIntrospector.java
+++ fixture-monkey-api/src/main/java/com/navercorp/fixturemonkey/api/introspector/OptionalIntrospector.java
@@ -48,7 +48,7 @@ public final class OptionalIntrospector implements ArbitraryIntrospector, Matche
 	}
 
 	@Override
-	@SuppressWarnings("optional:optional.parameter") // optional-parameter
+	@SuppressWarnings("optional:optional.parameter") // style-optional-parameter
 	public ArbitraryIntrospectorResult introspect(ArbitraryGeneratorContext context) {
 		ArbitraryProperty property = context.getArbitraryProperty();
 		if (!property.isContainer()) {
